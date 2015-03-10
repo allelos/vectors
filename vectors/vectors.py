83b4061fe48b0fb7ff46d63c6f1aaa733dfc7445
allelos <p.paliagkas@gmail.com>
2015-03-11 01:34:19 +0200

    class method from_list()

--- vectors/vectors.py
+++ vectors/vectors.py
@@ -4,13 +4,11 @@ class Vector(object):
     """ A simple vector toolikt dealing with vectors in the 3-dimensional
         space. """
 
-    def __init__(self, vector):
-        """ Argument vector is of type list. """
-
-        self.coordinates = vector  
-        self.x = vector[0]
-        self.y = vector[1]
-        self.z = vector[2]
+    def __init__(self, x, y, z):
+        self.vector = [x, y, z]
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __repr__(self):
         return 'Vector({0}, {1}, {2})'.format(self.x, self.y, self.z)
@@ -19,26 +17,26 @@ class Vector(object):
         """ Return a Vector instance as the product of the vector and a real
             number. """
 
-        return Vector([x * number for x in self.coordinates])
-    
+        return self.from_list([x * number for x in self.vector])
+
     def magnitude(self):
         """ Return magnitude of the vector. """
 
-        return (math.sqrt(reduce(lambda x,y: x+y, 
-                [x**2 for x in self.coordinates])))
+        return (math.sqrt(reduce(lambda x,y: x+y,
+                [x**2 for x in self.vector])))
 
     def add(self, vector):
         """ Return a Vector instance as the vector sum of two vectors. """
 
-        return (Vector([x+vector.coordinates[self.coordinates.index(x)]
-                for x in self.coordinates]))
+        return (self.from_list([x+vector.vector[self.vector.index(x)]
+                for x in self.vector]))
 
     def substract(self, vector):
         """ Return a Vector instance as the vector difference of two vectors.
         """
 
-        return (Vector([vector.coordinates[self.coordinates.index(x)]-x for x in
-                    self.coordinates]))
+        return (self.from_list([vector.vector[self.vector.index(x)]-x for x in
+                    self.vector]))
 
     def dot(self, vector, theta=None):
         """ Return the dot product of two vectors. If theta is given then the
@@ -49,17 +47,17 @@ class Vector(object):
             return (self.magnitude() * vector.magnitude() *
                     math.degress(math.cos(theta)))
         return (reduce(lambda x,y: x+y,
-                [x*vector.coordinates[self.coordinates.index(x)]
-                for x in self.coordinates]))
+                [x*vector.vector[self.vector.index(x)]
+                for x in self.vector]))
 
     def cross(self, vector):
         """ Return a Vector instance as the cross product of two vectors """
 
-        return Vector([(self.y * vector.z - self.z * vector.y),
+        return Vector((self.y * vector.z - self.z * vector.y),
                         (self.z * vector.x - self.x * vector.z),
-                        (self.x * vector.y - self.y * vector.x)])
+                        (self.x * vector.y - self.y * vector.x))
 
-    def angle(self, vector): 
+    def angle(self, vector):
         """ Return the angle between two vectors in degrees. """
 
         return (math.degrees(math.acos((self.dot(vector) / (self.magnitude() *
@@ -78,3 +76,10 @@ class Vector(object):
         if self.dot(vector) == 0:
             return True
         return False
+
+    @classmethod
+    def from_list(cls, l):
+        """ Return a Vector instance from a given list """
+
+        x, y, z = map(float, l)
+        return cls(x, y, z)
