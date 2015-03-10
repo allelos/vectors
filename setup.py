2935f143f61b0875419a56748eeb3363e83b9d06
allelos <p.paliagkas@gmail.com>
2015-03-10 21:53:00 +0200

    Initial commmit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+from setuptools import setup
+
+setup(
+    name = "vectors",
+    version = "0.0.1",
+    author = "Allelos",
+    author_email = "p.paliagkas@gmail.com",
+    description = "A simple vector toolkit dealing with vectors in the \
+        3-dimensional space",
+    license = "The MIT License (MIT)",
+    url = "https://github.com/allelos/vectors",
+    packages = ['vectors'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
