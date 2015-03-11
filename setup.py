bd52e044ea4545d6f0a6d18293274970daf065a3
allelos <p.paliagkas@gmail.com>
2015-03-11 02:38:22 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "vectors",
-    version = "0.0.3",
+    version = "0.0.6",
     author = "Allelos",
     author_email = "p.paliagkas@gmail.com",
     description = "A simple vector toolkit dealing with vectors in the \
