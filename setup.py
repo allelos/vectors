7f41e55096249cdda2325af4abb3d44b43053ffb
allelos <p.paliagkas@gmail.com>
2015-03-17 04:55:57 +0200

    Minor fixes

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import os
-
 from setuptools import setup, find_packages
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name = "vectors",
-    version = "0.0.8",
-    author = "Allelos",
-    author_email = "p.paliagkas@gmail.com",
-    description = "A simple vector toolkit dealing with vectors and points \
+    name="vectors",
+    version="0.0.9",
+    author="Allelos",
+    author_email="p.paliagkas@gmail.com",
+    description="A simple vector toolkit dealing with vectors and points \
         in the 3-dimensional space",
-    license = "The MIT License (MIT)",
-    url = "https://github.com/allelos/vectors",
-    packages = find_packages(),
-    classifiers = [
+    long_description=read('CHANGELOG.rst'),
+    license="The MIT License (MIT)",
+    url="https://github.com/allelos/vectors",
+    packages=find_packages(),
+    classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
